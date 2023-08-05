9449faafcf89da02904ddeb00466c52c823cbf7f
VickramN <Vickramshiv2004@gmail.com>
2023-08-05 15:06:02 -0400

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    const newArray =[];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] != remove){
+            newArray.push(array[i]);
+        } else{
+            continue;
+        }
+    }
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
